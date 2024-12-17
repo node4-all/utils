62769f480012d7df2b4dbffab254fd173f37d571
node4all <x4all.node@gmail.com>
2024-12-17 16:07:37 +0100

    Update go_installer.sh

--- go_installer.sh
+++ go_installer.sh
@@ -3,14 +3,14 @@
 # Parse command-line options
 while getopts ":v:" flag; do
   case "${flag}" in
-    v) V=${OPTARG} ;;
+    v) V="go${OPTARG}" ;;
     *) echo "WARN: unknown parameter: ${OPTARG}" ;;
   esac
 done
 
 # Determine the Go version
 LATEST_VERSION=$(curl -s https://go.dev/dl/?mode=json | jq -r '.[0].version')
-VERSION="go${V:-$LATEST_VERSION}"
+VERSION="${V:-$LATEST_VERSION}"
 
 # Determine the operating system and architecture
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
@@ -29,7 +29,7 @@ rm "${VERSION}.${OS}-${ARCH}.tar.gz"
 
 # Update PATH and GOPATH
 if ! grep -q "/usr/local/go/bin" "$HOME/.bash_profile"; then
-  echo "export PATH=\$PATH:/usr/local/go/bin" >> "$HOME/.bash_profile"
+  echo "export PATH=\$PATH:/usr/local/go/bin:~/go/bin" >> "$HOME/.bash_profile"
 fi
 if ! grep -q "GOPATH=\$HOME/go" "$HOME/.bash_profile"; then
   echo "export GOPATH=\$HOME/go" >> "$HOME/.bash_profile"
@@ -37,9 +37,6 @@ fi
 
 # Ensure .bash_profile exists
 [ ! -f ~/.bash_profile ] && touch ~/.bash_profile
-
-# Add Go binary directories to PATH and create ~/go/bin directory
-echo "export PATH=\$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
 [ ! -d ~/go/bin ] && mkdir -p ~/go/bin
 
 # Source .bash_profile to apply changes
